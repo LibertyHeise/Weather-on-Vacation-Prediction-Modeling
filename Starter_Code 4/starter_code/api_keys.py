a22aaad08d2e936a5eed0b0f995bfe0f425b0974
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-11-04 08:26:50 -0700

    updates

--- Starter_Code 4/starter_code/api_keys.py
+++ Starter_Code 4/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "ecb5febceb2c4dcd4f002b00b5c144cf"
 
-# Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+# Google Maps API Key
+google_maps = "AIzaSyAHbWqT0IakBqG33onXqZ1m5zOm_-eZJlo"
